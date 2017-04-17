38f9a0901a8a1afd3c8f69ae2781358be2688246
anibennnamdi <anibennnamdi@gmail.com>
2017-04-17 12:34:28 +0100

    Added a dynamic port

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ var express = require('express'),
     cors = require('cors'),
     server = require('http').createServer(app);
 
-
-
 mongoose.Promise = Promise;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 require('./app/approute/routes')(express, app);
-app.set('port', config.serverPort);
+var _port = process.env.PORT || config.serverPort;
+app.set('port', _port);
 
 server.listen(app.get('port'), function () {
     console.log('Node running on port', app.get('port'));
